fc72b60fb32835909bbdcd0094f41344b8d307e0
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2023-05-17 14:06:39 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  taskList = this.getData()
+  value = ''
+  getData() {
+    const data = localStorage.getItem('todolist')
+    return data ? JSON.parse(data) : []
+  }
+  onSubmit() {
+    this.taskList.push({ name: this.value })
+    this.value = ''
+    localStorage.setItem('todolist', JSON.stringify(this.taskList))
+  }
+  onClick(i: number) {
+    this.taskList.splice(i, 1)
+    localStorage.setItem('todolist', JSON.stringify(this.taskList))
+  }
+}
